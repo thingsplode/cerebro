d2d9c53fb11092bda8f16f81770f254d1979e6b0
Csaba Tamas <tamas.csaba@gmail.com>
2024-10-06 23:23:40 +0200

    full pipeline file added

--- main.py
+++ main.py
@@ -11,10 +11,9 @@ from tabulate import tabulate
 from dotenv import load_dotenv
 from src import set_project_folder
 from src.infra.qdrant import create_and_store_schema_embeddings
-from src.infra import fetch_relevant_tables_ddl
-from src.utils import table_info_to_ddl, save_db_info, load_all_db_info, extract_sql_from_markdown
-from src.connectors.pgres import scan_databases, execute_sql_query
-from src.llmops import generate_sql_with_ollama, improve_prompt, generate_refined_sql
+from src.utils import save_db_info
+from src.connectors.pgres import scan_databases
+from src.pipelines import execute_user_query_pipleline
 
 project_folder = dirname(__file__)
 set_project_folder(project_folder)
@@ -119,55 +118,7 @@ load_dotenv(join(project_folder, '.env'))
 #                 st.write('Insight:', bookmark['insight'])
 #                 st.bar_chart(bookmark['chart_data'].set_index(bookmark['chart_data'].columns[0]))
 
-def execute_user_query(query: str):
-    def execute_sql(sql):
-        # todo: chnage the database name to the one in the data source
-        rsp = execute_sql_query('dvdrental',sql)
-        if rsp["success"]:
-            if 'message' in rsp:
-                print(f"Query executed successfully. Rows affected: {rsp['message']}")
-            else:
-                table = tabulate(rsp['data'], headers=rsp['columns'], tablefmt='pretty')
-                print("Query results:")
-                print(table)
-        else:
-            logger.error(f"Error executing query: {rsp['error']}")
-        return rsp
-    
-    all_db_info = load_all_db_info(project_folder)
-    all_schema = list()
-    for db_name, db_info in all_db_info.items():
-        for table_name, table_info in db_info["tables"].items():
-            all_schema.append(table_info_to_ddl(db_name, table_name, table_info))
-    improved_query = improve_prompt(query)
-    relevant_tables = fetch_relevant_tables_ddl(improved_query)
-    full_schema = f"\n".join(all_schema)
-    llm_response = generate_sql_with_ollama(improved_query, relevant_tables, full_schema)
-    print(f"Generated SQL for query: {llm_response['response']}")
-    extracted_sqls = extract_sql_from_markdown(llm_response['response'])
-    for sql in extracted_sqls:
-        retry_count = 0
-        max_retries = 5
-        rsp = {"success": False}
-        current_sql = sql
-        while not rsp["success"] and retry_count < max_retries:
-            if retry_count == 0:
-                rsp = execute_sql(current_sql)
-            else:
-                improved_sql_query_response = generate_refined_sql(user_query=improved_query, 
-                                                    initial_sql=current_sql,
-                                                    relevant_schema=relevant_tables, 
-                                                    all_schema=full_schema, 
-                                                    context=llm_response['context'],
-                                                    error_message=rsp["error"])
-                logger.debug(f"Refined SQL: {improved_sql_query_response['response']}")
-                current_sql = extract_sql_from_markdown(improved_sql_query_response['response'])[0]
-                rsp = execute_sql(current_sql)
-                if not rsp["success"]:
-                    logger.error(f"Error executing query: {rsp}")
-            retry_count += 1
-            if retry_count == max_retries and not rsp["success"]:
-                logger.error(f"Failed to execute query after {max_retries} attempts.")
+
 def main():
     parser = argparse.ArgumentParser(description="Database management tool")
     subparsers = parser.add_subparsers(dest="action", help="Specify the action to perform")
@@ -186,7 +137,7 @@ def main():
         save_db_info(db_info, project_folder)
         create_and_store_schema_embeddings(db_info)
     elif args.action == "query" and args.query:
-        execute_user_query(args.query)
+        execute_user_query_pipleline(args.query)
     else:
         # Run the default Streamlit app if no command-line arguments are provided
         st.set_page_config(page_title="Data Insight Generator", layout="wide")
